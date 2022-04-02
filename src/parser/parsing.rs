78d6eb1c6f4622fa6c00d6c843a3e6b8059606ee
Yaxin Cheng <Yaxin.Cheng@iCloud.com>
2022-04-01 21:45:26 -0400

    Add read helper functions to Parser

--- src/parser/parsing.rs
+++ src/parser/parsing.rs
@@ -7,7 +7,7 @@ use crate::Header;
 use std::collections::BTreeMap;
 use std::io::{BufReader, Read, Result, Seek, SeekFrom};
 
-pub struct Parser<R: Read + Seek>(BufReader<R>);
+pub struct Parser<R: Read>(BufReader<R>);
 
 const ENTRY_FLAG_COMPLEX: u16 = 0x0001;
 
@@ -17,9 +17,8 @@ impl<R: Read + Seek> Parser<R> {
     }
 
     pub fn parse(&mut self) -> Result<Vec<Package>> {
-        let header = Header::try_from(&mut self.0)?;
-        println!("{header:?}");
-        let package_count = read_util::read_u32(&mut self.0)? as usize;
+        let _header = Header::try_from(&mut self.0)?;
+        let package_count = self.read_u32()? as usize;
         let mut packages = Vec::with_capacity(package_count);
         for _ in 0..package_count {
             packages.push(self.parse_package()?)
@@ -30,15 +29,15 @@ impl<R: Read + Seek> Parser<R> {
     fn parse_package(&mut self) -> Result<Package> {
         let global_string_pool = self.parse_string_pool()?;
         let package_header = Header::try_from(&mut self.0)?;
-        assert_eq!(package_header.type_flag, TypeFlag::RES_TABLE_PACKAGE_TYPE);
-        let package_id = read_util::read_u32(&mut self.0)?;
+        debug_assert_eq!(package_header.type_flag, TypeFlag::RES_TABLE_PACKAGE_TYPE);
+        let package_id = self.read_u32()?;
         let package_name = self.parse_package_name()?;
 
-        let _type_string_offset = read_util::read_u32(&mut self.0)?;
-        let _last_public_type = read_util::read_u32(&mut self.0)?;
-        let _key_string_offset = read_util::read_u32(&mut self.0)?;
-        let _last_public_key = read_util::read_u32(&mut self.0)?;
-        let _type_id_offset = read_util::read_u32(&mut self.0)?;
+        let _type_string_offset = self.read_u32()?;
+        let _last_public_type = self.read_u32()?;
+        let _key_string_offset = self.read_u32()?;
+        let _last_public_key = self.read_u32()?;
+        let _type_id_offset = self.read_u32()?;
 
         let type_names = self.parse_string_pool()?;
         let mut types = Types::from(type_names);
@@ -77,14 +76,14 @@ impl<R: Read + Seek> Parser<R> {
     }
 
     fn parse_specs(&mut self, types: &mut Types) -> Result<()> {
-        let type_id = read_util::read_u8(&mut self.0)? as usize;
-        let res0 = read_util::read_u8(&mut self.0)?;
-        let res1 = read_util::read_u16(&mut self.0)?;
-        let entry_count = read_util::read_u32(&mut self.0)? as usize;
+        let type_id = self.read_u8()? as usize;
+        let res0 = self.read_u8()?;
+        let res1 = self.read_u16()?;
+        let entry_count = self.read_u32()? as usize;
 
         let target_type = &mut types.types[type_id - 1];
         for spec_id in 0..entry_count {
-            let flags = read_util::read_u32(&mut self.0)?;
+            let flags = self.read_u32()?;
             target_type.specs.push(ResSpec {
                 res0,
                 res1,
@@ -98,11 +97,11 @@ impl<R: Read + Seek> Parser<R> {
 
     fn parse_config(&mut self, types: &mut Types) -> Result<()> {
         let start_pos = self.0.stream_position()? - 8;
-        let type_id = read_util::read_u8(&mut self.0)? as usize;
-        let res0 = read_util::read_u8(&mut self.0)?;
-        let res1 = read_util::read_u16(&mut self.0)?;
-        let entry_count = read_util::read_u32(&mut self.0)? as usize;
-        let entry_start = read_util::read_u32(&mut self.0)? as u64;
+        let type_id = self.read_u8()? as usize;
+        let res0 = self.read_u8()?;
+        let res1 = self.read_u16()?;
+        let entry_count = self.read_u32()? as usize;
+        let entry_start = self.read_u32()? as u64;
         let config_id = self.parse_config_id()?;
 
         let resource_type = &mut types.types[type_id - 1];
@@ -121,7 +120,7 @@ impl<R: Read + Seek> Parser<R> {
 
     fn parse_config_id(&mut self) -> Result<Vec<u8>> {
         let position = self.0.stream_position()?;
-        let size = read_util::read_u32(&mut self.0)? as usize;
+        let size = self.read_u32()? as usize;
         let mut config_id = vec![0_u8; size];
         self.0.seek(SeekFrom::Start(position))?;
         self.0.read_exact(&mut config_id)?;
@@ -135,7 +134,7 @@ impl<R: Read + Seek> Parser<R> {
         entry_count: usize,
         res_type: &mut Type,
     ) -> Result<BTreeMap<usize, ResourceEntry>> {
-        let entries = std::iter::repeat_with(|| read_util::read_i32(&mut self.0))
+        let entries = std::iter::repeat_with(|| self.read_i32())
             .take(entry_count)
             .collect::<Result<Vec<_>>>()?;
         let mut resources = BTreeMap::new();
@@ -145,9 +144,9 @@ impl<R: Read + Seek> Parser<R> {
             }
             self.0
                 .seek(SeekFrom::Start(start_pos + entry_start + entry as u64))?;
-            let _size = read_util::read_u16(&mut self.0)?;
-            let flags = read_util::read_u16(&mut self.0)?;
-            res_type.specs[spec_index].name_index = read_util::read_u32(&mut self.0)? as usize;
+            let _size = self.read_u16()?;
+            let flags = self.read_u16()?;
+            res_type.specs[spec_index].name_index = self.read_u32()? as usize;
             let resource = self.parse_res_entry(flags, spec_index)?;
             resources.insert(spec_index, resource);
         }
@@ -156,11 +155,11 @@ impl<R: Read + Seek> Parser<R> {
 
     fn parse_res_entry(&mut self, flags: u16, spec_id: usize) -> Result<ResourceEntry> {
         let value = if flags & ENTRY_FLAG_COMPLEX != 0 {
-            let parent = read_util::read_u32(&mut self.0)?;
-            let count = read_util::read_u32(&mut self.0)? as usize;
+            let parent = self.read_u32()?;
+            let count = self.read_u32()? as usize;
             let mut values = Vec::with_capacity(count);
             for _ in 0..count {
-                let index = read_util::read_u32(&mut self.0)?;
+                let index = self.read_u32()?;
                 let value = Value::try_from(&mut self.0)?;
                 values.push((index, value));
             }
@@ -174,4 +173,21 @@ impl<R: Read + Seek> Parser<R> {
             value,
         })
     }
+}
+
+macro_rules! impl_read {
+    ($type: ty) => {
+        paste::paste! {
+            fn [<read_ $type>](&mut self) -> Result<$type> {
+                read_util::[<read_ $type>](&mut self.0)
+            }
+        }
+    };
+}
+
+impl<R: Read> Parser<R> {
+    impl_read!(u32);
+    impl_read!(u16);
+    impl_read!(u8);
+    impl_read!(i32);
 }
